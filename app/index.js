0442c9cb6292f2a7d2eab89dc76d3e1ebd361423
MichaelMammoliti <mammoliti.michael@gmail.com>
2019-02-07 09:04:27 +0000

    add css loaders

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 import App from './app.js';
+import styles from './styles/main.scss';
 
 const wrapper = document.getElementById('wrapper');
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,15 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: './app/index.js',
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/',
   },
+
   mode: 'development',
+
   module: {
     rules: [
       {
@@ -19,15 +22,32 @@ module.exports = {
             loader: 'babel-loader'
           }
         ]
-      }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]--[hash:base64:5]',
+            }
+          },
+          { loader: 'sass-loader' },
+        ]
+      },
     ]
   },
+
   plugins: [
     new webpack.ProvidePlugin({
       React: 'react',
       ReactDOM: 'react-dom',
     }),
   ],
+
   devServer: {
     port: 9000,
     open: true,
