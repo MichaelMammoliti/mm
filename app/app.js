eafe33e925f392cc2d4efb299a058011778100aa
MichaelMammoliti <mammoliti.michael@gmail.com>
2019-02-07 19:43:05 +0000

    - render the navigation \in app.js

--- app/app.js
+++ app/app.js
@@ -1,3 +1,24 @@
-const App = () => <div>hello</div>;
+import { Navigation } from './components';
+
+const navigationItems = [
+  {
+    text: 'who I am',
+  },
+  {
+    text: 'experience',
+  },
+  {
+    text: 'interests',
+  },
+  {
+    text: 'contacts',
+  },
+];
+
+const App = () => (
+  <div>
+    <Navigation orientation='vertical' type='circle' items={navigationItems} />
+  </div>
+);
 
 export default App;
